5640186e8aaef0eb1d6dbfac3387c80572bfe09c
Aditya Dubey <adityadubey9880@gmail.com>
2026-01-19 11:53:18 +0530

    Revision

---
+++ Revision/src/main/java/com/example/revision/Hello.java
@@ -0,0 +1,9 @@
+package com.example.revision;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Hello {
+    @GetMapping("/")
+    public String index() {return "This is revision day!";}
+}
