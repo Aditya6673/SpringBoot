9fc5b23330a6d788e2a4e59b6f3f3a18f49f6312
Aditya Dubey <adityadubey9880@gmail.com>
2026-01-19 15:24:28 +0530

    Added five data

--- Day2Part2/src/main/java/com/example/day2part2/controller/Home.java
+++ Day2Part2/src/main/java/com/example/day2part2/controller/Home.java
@@ -4,11 +4,19 @@ import com.example.day2part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class Home {
     @GetMapping("/")
-    public StudentModel getStudent() {
-        StudentModel student = new StudentModel(1,"Aditya","aditya1040@zohomail.in");
-        return student;
+    public List<StudentModel> getStudent() {
+        List<StudentModel> list = new ArrayList<>();
+        list.add(new StudentModel(1,"Aditya","aditya1040@zohomail.in"));
+        list.add(new StudentModel(2,"Abhishek","abhishek@zohomail.in"));
+        list.add(new StudentModel(3,"Ajay","ajay@zohomail.in"));
+        list.add(new StudentModel(4,"Agrim","agrim@zohomail.in"));
+        list.add(new StudentModel(5,"Aditya","aditya@zohomail.in"));
+        return list;
     }
 }
